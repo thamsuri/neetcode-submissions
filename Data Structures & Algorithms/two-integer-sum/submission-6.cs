a674ba18d682fddbb89f9d6adb9b2c7d2686ad75
thamsuri <49010384+thamsuri@users.noreply.github.com>
2026-07-06 07:31:07 -0400

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+
+        Dictionary<int,int> map = new Dictionary<int,int>();
+
+        for (int i=0; i<nums.Length;i++){
+            int complement = target-nums[i];
+
+            if (map.ContainsKey(complement)){
+                return new int[] { map[complement],i};
+            }
+            else
+            {
+                map[nums[i]]=i;
+            }
+        }
+
+        return new int[] {0};
+    }
+}
