b5f0549e02cd3d465c9e55ccc2a55dbf68cb01a3
thamsuri <49010384+thamsuri@users.noreply.github.com>
2026-07-25 13:26:32 -0400

    Add: climbing-stairs - submission-9

---
+++ Data Structures & Algorithms/climbing-stairs/submission-9.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int ClimbStairs(int n) {  
+
+        if (n <= 2) return n;
+        
+        int[] table = new int[n+1];
+        Array.Fill(table, 0)   ;        
+
+        table[1] = 1;
+        table[2] = 2;
+
+        for (int i = 3;i<=n;i++){
+            table[i] = table[i-1]+table[i-2];
+        }
+        return table[n];
+        
+    }
+}
