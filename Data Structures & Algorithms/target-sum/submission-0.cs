47451bfdc4dbbedf4edc3dec23cd0386bd8b9315
thamsuri <49010384+thamsuri@users.noreply.github.com>
2026-07-26 21:07:22 -0400

    Add: target-sum - submission-0

---
+++ Data Structures & Algorithms/target-sum/submission-0.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int FindTargetSumWays(int[] nums, int target) {
+
+        return helper (0,0,nums,target);
+        
+    }
+
+    public int helper(int i, int current_sum,int[] nums,int target){
+        if (i == nums.Length){
+            if (current_sum == target){
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        return (helper (i+1,current_sum+nums[i],nums,target) +    helper (i+1,current_sum-nums[i],nums,target));
+    }
+}
